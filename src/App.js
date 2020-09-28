fc294c5d85544d2db080575ac56d2669f92fab08
Nguyen Ngo <locnguyen1986@gmail.com>
2020-09-28 15:03:41 +0700

    add new code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bye there !!! 12333 aloha,
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1234
         </a>
       </header>
     </div>
